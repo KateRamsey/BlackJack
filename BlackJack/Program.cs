5e0cd83153ef980ea2b54f1945aab2b17773c134
Kate Ramsey <kateramsey@live.com>
2016-03-03 14:58:26 -0600

    Layout game logic in comments

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -10,6 +10,50 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
+            //create deck
+            Player Human = new Player();
+            Player Dealer = new Player();
+            Human.isDealer = false;
+            Dealer.isDealer = true;
+
+
+            //Game Loop
+
+            //shuffle deck
+
+            //deal 2 cards to player
+            //deal 2 cards to dealer
+
+            //show player cards
+            //show dealer card #1
+
+            //calculate and display player score
+            //if player score == 21, player wins, skip to end
+
+            //while player's turn
+            //if player score > 21 they lose, skip to end
+            //else ask to hit or stay, if stay dealers turn, else deal
+            //calculate and display player score
+
+
+            //Dealer's turn
+            //show hidden card
+            //calculate and display score
+            //check win
+            //check lose
+            //while dealer's score < 16
+                //hit
+                //calculate and display score
+                //check win
+                //check lose
+
+
+            //Calculate and display winner
+
+            //ask "Play again?"
+
+
+            Console.ReadLine();
         }
     }
 }
