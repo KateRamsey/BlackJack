e73bd9468d2c4bba6ee149ffb50c0befe7673d3d
Kate Ramsey <kateramsey@live.com>
2016-03-03 14:38:38 -0600

    Add Card class

---
+++ CardClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Card
+{
+	public Card()
+	{
+	}
+
+    //suit (enum?)
+    //rank (enum?)
+
+    public int Value()
+    {
+        switch (this.rank)
+        {
+            // case Ace return 11
+            //case king return 10
+            //etc
+        }
+    }
+}
