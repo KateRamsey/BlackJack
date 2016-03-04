6d0f830a4ef095dd672ec0884dc098fcf3ca9a54
Kate Ramsey <kateramsey@live.com>
2016-03-04 14:36:19 -0600

    Add keep playing loop

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -10,8 +10,6 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
-
-
             List<Card> Deck = new List<Card>();
             foreach (Rank r in Enum.GetValues(typeof(Rank)))
             {
@@ -25,43 +23,60 @@ namespace BlackJack
             Player Human = new Player(false);
             Player Dealer = new Player(true);
 
+            bool StillPlaying = true;
+            char PlayAgain;
+
             //Game Loop
+            while (StillPlaying)
+            {
+
+                Console.WriteLine("Let's Play!!");
+                //deal 2 cards to player
+                //deal 2 cards to dealer
+
+                //show player cards
+                //show dealer card #1
+
+                //calculate and display player score
+                //if player score == 21, player wins, skip to end
+
+                //while player's turn
+                //if player score > 21 they lose, skip to end
+                //else ask to hit or stay, if stay dealers turn, else deal
+                //calculate and display player score
 
 
-
-            //deal 2 cards to player
-            //deal 2 cards to dealer
-
-            //show player cards
-            //show dealer card #1
-
-            //calculate and display player score
-            //if player score == 21, player wins, skip to end
-
-            //while player's turn
-            //if player score > 21 they lose, skip to end
-            //else ask to hit or stay, if stay dealers turn, else deal
-            //calculate and display player score
+                //Dealer's turn
+                //show hidden card
+                //calculate and display score
+                //check win
+                //check lose
+                //while dealer's score < 16
+                //hit
+                //calculate and display score
+                //check win
+                //check lose
 
 
-            //Dealer's turn
-            //show hidden card
-            //calculate and display score
-            //check win
-            //check lose
-            //while dealer's score < 16
-            //hit
-            //calculate and display score
-            //check win
-            //check lose
+                //Calculate and display winner
 
-
-            //Calculate and display winner
-
-            //ask "Play again?"
-
-            //shuffle deck if play again
-            randomDeck = Deck.OrderBy(x => Guid.NewGuid()).ToList();
+                Console.WriteLine("Would you like to play again? Please press 'Y' or 'N'");
+                PlayAgain = char.Parse(Console.ReadLine());
+                while(PlayAgain != 'Y' && PlayAgain != 'y' && PlayAgain != 'n' && PlayAgain != 'N')
+                {
+                    Console.WriteLine("Please press 'Y' or 'N'");
+                    PlayAgain = char.Parse(Console.ReadLine());
+                }
+                if (PlayAgain == 'N' || PlayAgain == 'n')
+                {
+                    StillPlaying = false;
+                    Console.WriteLine("Thanks for playing, have a great day!");
+                }
+                else
+                {
+                    randomDeck = Deck.OrderBy(x => Guid.NewGuid()).ToList();
+                }
+            }
 
             Console.ReadLine();
         }
